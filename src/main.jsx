cb13e00b1ca39fe45c8365f8be6a466e3c21da42
Shashank <shashanksagar398@gmail.com>
2025-08-01 03:04:51 +0530

    fix: Update import statement for index.css to use simple CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css"; // ← Simple CSS instead of SCSS
+import "./index.css"; 
 import App from "./App.jsx";
 import { App as AntdApp } from "antd";
 import "@ant-design/v5-patch-for-react-19";
